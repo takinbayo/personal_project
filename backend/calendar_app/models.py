fafed54cd5065549ee66caa857a7e85c88858c6f
tomiwa <tmakinbayo@gmail.com>
2023-10-15 12:49:26 -0500

    db made

--- backend/calendar_app/models.py
+++ backend/calendar_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    user_email = models.EmailField()
+    
+    def __str__(self):
+        return self.first_name
+
+class Event(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    event_name = models.CharField(max_length=200)
+    event_des = models.TextField()
+    location = models.TextField()
+    date = models.DateField(default="2023-12-25")
+    time = models.TimeField(default="23:00")
+
+    def __str__(self):
+        return self.event_name
